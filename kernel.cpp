faa32adc8fada3e913acda2df2b61c807cc1eb66
Kdy010010 <137839917+Kdy010010@users.noreply.github.com>
2023-12-26 14:33:32 +0900

    Create kernel.cpp

---
+++ kernel.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    std::cout << "Main.exe를 실행합니다." << std::endl;
+    system("Main.exe"); // 시스템 명령을 사용하여 Main.exe를 실행합니다.
+    return 0;
+}
