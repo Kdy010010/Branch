5ddf565d90ebdfe01d6d0a872c1b58098bff179c
Kdy010010 <137839917+Kdy010010@users.noreply.github.com>
2023-12-27 18:54:18 +0900

    Update kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -2,7 +2,9 @@
 #include <cstdlib>
 
 int main() {
-    std::cout << "Main.exe를 실행합니다." << std::endl;
+    std::cout << "Powered by BRANCH os engine" << std::endl;
+    std::cout << "Loading main kernel." << std::endl;
+    std::cout << "BRANCH project by KS foundation" << std::endl;
     system("Main.exe"); // 시스템 명령을 사용하여 Main.exe를 실행합니다.
     return 0;
 }
